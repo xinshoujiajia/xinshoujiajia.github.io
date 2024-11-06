4caaedf10a78444b861e19eecaebf6d299b93ce9
xinshoujiajia <xinshoujiajia123.163.com>
2024-11-06 23:31:54 +0800

    Site updated: 2024-11-06 23:31:54

--- js/fa.js
+++ js/fa.js
@@ -1,47 +1,77 @@
-const arr=[
-    {name:"fa_0",len:"50%",index:10,root_count:1,branch_count:2},
-    {name:"fa_1",len:"25%",index:6,root_count:1,branch_count:2},
-    {name:"fa_2",len:"25%",index:7,root_count:4,branch_count:3},
-    {name:"fa_3",len:"25%",index:10,root_count:1,branch_count:2},
-    {name:"fa_4",len:"25%",index:10,root_count:1,branch_count:2}
-];
-const current=arr[Math.floor(arr.length*Math.random())];
-
-function includeLinkStyle(url){
-    var link=document.createElement("link");
-    link.rel="stylesheet";
-    link.type="text/css";
-    link.href=url;
-    document.getElementsByTagName("head")[0].appendChild(link);
+function createRule(value){
+    rule=new Array(10);
+    for(let i=0;i!=10;i++){
+        rule[i]=(value%4);
+        value>>=2;
+    }
+    return rule;
 }
-window.onload = () => {
-    const body = document.body;
-    const SVG_NS = "http://www.w3.org/2000/svg";
-    const svg = document.createElementNS(SVG_NS,"svg");
 
-    includeLinkStyle("/css/fa/fa.css");
-    includeLinkStyle("/css/fa/"+current.name+".css");
+function getRandomInt(max) {
+    max = Math.floor(max);
+    return Math.floor(Math.random() *max);
+}
 
-    function fa(n,root,tag) {
-        if(n==0)return;
-        var group=document.createElementNS(SVG_NS,"g");
-        var line=document.createElementNS(SVG_NS,"line");
-        line.setAttribute("x1","0%");
-        line.setAttribute("y1","0%");
-        line.setAttribute("x2","0%");
-        line.setAttribute("y2",current.len);
-        line.setAttribute("class",current.name+"_l_"+n);
-        group.setAttribute("class",current.name+"_g_"+tag);
-        group.appendChild(line);
-        root.appendChild(group);
-        for(let i=0;i!=current.branch_count;++i){
-            fa(n-1,group,i);
+const value=getRandomInt(1048576);
+window.addEventListener('resize', resizeCanvas, false);
+window.addEventListener('load', ()=>{
+    const canvas=document.createElement("canvas");
+    var subtitle=document.getElementsByClassName("site-subtitle");
+    subtitle[0].textContent=`规则：${value}`;
+    canvas.id="canvas";
+    document.body.appendChild(canvas);
+    resizeCanvas();}, false);
+ 
+function resizeCanvas() {
+    const canvas = document.getElementById('canvas');
+    const displayWidth = window.innerWidth;
+    const displayHeight = window.innerHeight;
+ 
+    if (canvas.width !== displayWidth || canvas.height !== displayHeight) {
+        const width=displayWidth;
+        const height=displayHeight;
+
+        canvas.width = width;
+        canvas.height = height;
+ 
+        const ctx = canvas.getContext('2d');
+	const imageData = ctx.getImageData(0, 0, width, height);
+        const data=imageData.data;
+
+        const rule=createRule(value);
+        let pre=new Array(width).fill(0);
+        let cur=new Array(width);
+        let color=0;
+        pre[width>>1]=3;
+        let index=0;
+        for(let i=0;i!=height;i++){
+            let j=0;
+            cur[j]=rule[pre[0]+pre[1]];
+            color=28*cur[j];
+            data[index++]=color;
+            data[index++]=color;
+            data[index++]=color;
+            data[index++]=255;
+
+            while(++j!=width-1){
+                cur[j]=rule[pre[j-1]+pre[j]+pre[j+1]];
+                color=28*cur[j];
+                data[index++]=color;
+                data[index++]=color;
+                data[index++]=color;
+                data[index++]=255;
+            }
+
+            cur[j]=rule[pre[j]+pre[j-1]];
+            color=28*cur[j];
+            data[index++]=color;
+            data[index++]=color;
+            data[index++]=color;
+            data[index++]=255;
+
+            pre=Array.from(cur);
         }
+        ctx.putImageData(imageData, 0, 0);
     }
-
-    for(let i=0;i!=current.root_count;++i){
-        fa(current.index,svg,"root_"+i);
-    }
-
-    body.appendChild(svg);
 }
+
